73cf35c92ff3b60993e85538edd2359cf076fd0e
NonoFL <FL_suunshine@163.com>
2021-10-07 13:34:46 +0800

    add config

--- multi_train.sh
+++ multi_train.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-python  demo/image_demo.py demo/vhr/084.jpg configs/fcos/fcos_r50_caffe_fpn_gn-head_1x_vhrvoc.py work_dirs/fcos_r50_caffe_fpn_gn-head_1x_vhrvoc/epoch_40.pth
-
-python  demo/image_demo.py demo/vhr/188.jpg configs/fcos/fcos_r50_caffe_fpn_gn-head_1x_vhrvoc.py work_dirs/fcos_r50_caffe_fpn_gn-head_1x_vhrvoc/epoch_40.pth
-
-python  demo/image_demo.py demo/vhr/181.jpg configs/fcos/fcos_r50_caffe_fpn_gn-head_1x_vhrvoc.py work_dirs/fcos_r50_caffe_fpn_gn-head_1x_vhrvoc/epoch_40.pth
-
-python  demo/image_demo.py demo/vhr/279.jpg configs/fcos/fcos_r50_caffe_fpn_gn-head_1x_vhrvoc.py work_dirs/fcos_r50_caffe_fpn_gn-head_1x_vhrvoc/epoch_40.pth
-
-python  demo/image_demo.py demo/vhr/523.jpg configs/fcos/fcos_r50_caffe_fpn_gn-head_1x_vhrvoc.py work_dirs/fcos_r50_caffe_fpn_gn-head_1x_vhrvoc/epoch_40.pth
-
-python  demo/image_demo.py demo/vhr/549.jpg configs/atss/atss_r50_fpn_1x_vhrvoc.py work_dirs/fcos_r50_caffe_fpn_gn-head_1x_vhrvoc/epoch_40.pth
-
+python tools/train.py configs/atss/atss_r50_myfpn_1x_ssdd_v6.py
+python tools/train.py configs/atss/atss_r50_myfpn_1x_vhrvoc_v6.py
+python tools/train.py configs/atss/atss_r50_myfpn_1x_vhrvoc_v6_lrchange.py
