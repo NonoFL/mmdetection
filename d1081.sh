ada206e7ba5127da886915b78e410e0fc7efa723
NonoFL <FL_suunshine@163.com>
2021-10-08 19:01:25 +0800

    nothing new

--- d1081.sh
+++ d1081.sh
@@ -7,10 +7,10 @@ python tools/train.py configs/atss/myfpn/atss_r50_myfpn-1_1x_vhrvoc_v6.py
 
 python tools/train.py configs/atss/myfpnv4/atss_r50_myfpnv4_1x_ssdd_v6.py
 python tools/train.py configs/atss/myfpnv4/atss_r50_myfpnv4_1x_vhrvoc_v6.py
-python tools/train.py configs/atss/myfpnv4/atss_r50_myfpnv4-1_1x_ssdd_v6.py
-python tools/train.py configs/atss/myfpnv4/atss_r50_myfpnv4-1_1x_vhrvoc_v6.py
+# python tools/train.py configs/atss/myfpnv4/atss_r50_myfpnv4-1_1x_ssdd_v6.py
+# python tools/train.py configs/atss/myfpnv4/atss_r50_myfpnv4-1_1x_vhrvoc_v6.py
 
-python tools/train.py configs/atss/myfpnv5/atss_r50_myfpnv5_1x_ssdd_v6.py
-python tools/train.py configs/atss/myfpnv5/atss_r50_myfpnv5_1x_vhrvoc_v6.py
-python tools/train.py configs/atss/myfpnv5/atss_r50_myfpnv5-1_1x_ssdd_v6.py
-python tools/train.py configs/atss/myfpnv5/atss_r50_myfpnv5-1_1x_vhrvoc_v6.py
+# python tools/train.py configs/atss/myfpnv5/atss_r50_myfpnv5_1x_ssdd_v6.py
+# python tools/train.py configs/atss/myfpnv5/atss_r50_myfpnv5_1x_vhrvoc_v6.py
+# python tools/train.py configs/atss/myfpnv5/atss_r50_myfpnv5-1_1x_ssdd_v6.py
+# python tools/train.py configs/atss/myfpnv5/atss_r50_myfpnv5-1_1x_vhrvoc_v6.py
