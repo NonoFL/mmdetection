e77dfae296b56a27e22f3c239fadb6791ac5f05f
NonoFL <FL_suunshine@163.com>
2021-10-22 19:53:45 +0800

    nono

---
+++ atssv6_topk_bu.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+python tools/train.py configs/atss/atss_assignv6/atss_r50_fpn_1x_ssdd_v6_topk5.py
+python tools/train.py configs/atss/atss_assignv6/atss_r50_fpn_1x_ssdd_v6_topk6.py
+python tools/train.py configs/atss/atss_assignv6/atss_r50_fpn_1x_ssdd_v6_topk8.py
+python tools/train.py configs/atss/atss_assignv6/atss_r50_fpn_1x_ssdd_v6_topk12.py
+python tools/train.py configs/atss/atss_assignv6/atss_r50_fpn_1x_ssdd_v6_topk14.py
+python tools/train.py configs/atss/atss_assignv6/atss_r50_fpn_1x_ssdd_v6_topk15.py
+python tools/train.py configs/atss/atss_assignv6/atss_r50_fpn_1x_vhrvoc_v6_topk6.py
+python tools/train.py configs/atss/atss_assignv6/atss_r50_fpn_1x_vhrvoc_v6_topk5.py
+python tools/train.py configs/atss/atss_assignv6/atss_r50_fpn_1x_vhrvoc_v6_topk7.py
+python tools/train.py configs/atss/atss_assignv6/atss_r50_fpn_1x_vhrvoc_v6_topk8.py
+python tools/train.py configs/atss/atss_assignv6/atss_r50_fpn_1x_vhrvoc_v6_topk10.py
+python tools/train.py configs/atss/atss_assignv6/atss_r50_fpn_1x_vhrvoc_v6_topk12.py
+python tools/train.py configs/atss/atss_assignv6/atss_r50_fpn_1x_vhrvoc_v6_topk14.py
+python tools/train.py configs/atss/atss_assignv6/atss_r50_fpn_1x_vhrvoc_v6_topk15.py
+
+
+# MYPFN——free:
+python tools/train.py configs/atss/myfpn/atss_r50_myfpn_1x_ssdd_v6-topk6.py
+python tools/train.py configs/atss/myfpn/atss_r50_myfpn_1x_ssdd_v6-topk7.py
+python tools/train.py configs/atss/myfpn/atss_r50_myfpn_1x_ssdd_v6-topk8.py
+python tools/train.py configs/atss/myfpn/atss_r50_myfpn_1x_ssdd_v6-topk10.py
+python tools/train.py configs/atss/myfpn/atss_r50_myfpn_1x_ssdd_v6-topk11.py
+python tools/train.py configs/atss/myfpn/atss_r50_myfpn_1x_ssdd_v6-topk12.py
+python tools/train.py configs/atss/myfpn/atss_r50_myfpn_1x_vhrvoc_v6-topk14.py
+python tools/train.py configs/atss/myfpn/atss_r50_myfpn_1x_vhrvoc_v6-topk15.py
---
+++ fpn12_free_10.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+python tools/train.py configs/atss/myfpnv12/atss_r50_myfpnv12_1_10_1x_ssdd_v6.py
+python tools/train.py configs/atss/myfpnv12/atss_r50_myfpnv12_1_10_1x_vhrvoc_v6.py
